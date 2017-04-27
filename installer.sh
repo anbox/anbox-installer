f479fbd9eaa80361f0489ba074a26e4397603167
Simon Fels <morphis@gravedo.de>
2017-04-27 06:49:09 +0200

    Purge ppa-purge if we installed it

--- installer.sh
+++ installer.sh
@@ -79,8 +79,16 @@ uninstall() {
 	sudo snap remove anbox
 	sudo apt purge -y anbox-modules-dkms
 	if [ -e /etc/apt/sources.list.d/morphis-ubuntu-anbox-support-xenial.list ]; then
+		ppa_purged_installed=0
+		if ! dpkg --get-selections | grep -q ppa-purge ; then
+			sudo apt install -y ppa-purge
+			ppa_purged_installed=1
+		fi
 		sudo apt install -y ppa-purge
 		sudo ppa-purge ppa:morphis/anbox-support
+		if [ "$ppa_purged_installed" -eq 1 ]; then
+			sudo apt purge ppa-purge
+		fi
 	fi
 	set +x
 }
